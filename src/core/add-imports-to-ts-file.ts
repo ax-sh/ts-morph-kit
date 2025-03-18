8e21cff8f8d5b39e625424e834943a43870993e7
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-18 17:26:17 +0545

    feat: add function to manage TypeScript imports

---
+++ src/core/add-imports-to-ts-file.ts
@@ -0,0 +1,12 @@
+import type { TsFileImports } from "../utils/add-imports.ts"
+import {
+  addImportsToSourceFile,
+} from "../utils/add-imports.ts"
+import { openAsSourceFile } from "./create-project.ts"
+
+export function addImportsToTsFile(tsFile: string, imports: TsFileImports[]) {
+  const sourceFile = openAsSourceFile(tsFile)
+
+  addImportsToSourceFile(sourceFile, imports)
+  return sourceFile
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-import type { TsFileImports } from "./utils/add-imports.ts"
-
-import { openAsSourceFile } from "./core/create-project.ts"
-import { addImportsToSourceFile } from "./utils/add-imports.ts"
+export { addImportsToTsFile } from "./core/add-imports-to-ts-file.ts"
 
 export { createProject, openAsSourceFile } from "./core/create-project.ts"
 
@@ -9,10 +6,3 @@ export { findDefaultExport } from "./utils/find-default-export.ts"
 
 export { getFunctionNameFromExpression } from "./utils/get-function-name-from-expression.ts"
 export { createTestSourceFile } from "./utils/test-utils.ts"
-
-export function addImportsToTsFile(tsFile: string, imports: TsFileImports[]) {
-  const sourceFile = openAsSourceFile(tsFile)
-
-  addImportsToSourceFile(sourceFile, imports)
-  return sourceFile
-}
