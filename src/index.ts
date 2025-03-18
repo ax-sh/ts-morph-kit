42dea10c4128e15ddbd538e51dddc468aea3d3d3
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-18 16:23:51 +0545

    ci: format files before release

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export { createProject, openAsSourceFile } from "./core/create-project.ts"
+export { createProject, openAsSourceFile } from "./core/create-project.ts";
 
-export { addImports, addImportsToSourceFile } from "./utils/add-imports.ts"
+export { addImports, addImportsToSourceFile } from "./utils/add-imports.ts";
 
-export { findDefaultExport } from "./utils/find-default-export.ts"
+export { findDefaultExport } from "./utils/find-default-export.ts";
 
-export { getFunctionNameFromExpression } from "./utils/get-function-name-from-expression.ts"
-export { createTestSourceFile } from "./utils/test-utils.ts"
+export { getFunctionNameFromExpression } from "./utils/get-function-name-from-expression.ts";
+export { createTestSourceFile } from "./utils/test-utils.ts";
--- src/utils/add-imports.ts
+++ src/utils/add-imports.ts
@@ -1,9 +1,9 @@
-import type { SourceFile } from "ts-morph"
+import type { SourceFile } from "ts-morph";
 
 interface TsImports {
-  namedImports?: string[]
-  defaultImport?: string
-  moduleSpecifier: string
+  namedImports?: string[];
+  defaultImport?: string;
+  moduleSpecifier: string;
 }
 
 /**
@@ -24,40 +24,39 @@ interface TsImports {
  */
 export function addImports(sourceFile: SourceFile, imports: TsImports[]) {
   for (const importInfo of imports) {
-    const { namedImports, defaultImport, moduleSpecifier } = importInfo
+    const { namedImports, defaultImport, moduleSpecifier } = importInfo;
 
     // Check if an import for the module already exists
     const existingImport = sourceFile
       .getImportDeclarations()
-      .find(imp => imp.getModuleSpecifierValue() === moduleSpecifier)
+      .find((imp) => imp.getModuleSpecifierValue() === moduleSpecifier);
 
     if (existingImport) {
       // Merge named imports if they exist
       if (namedImports) {
         const existingNamedImports = existingImport
           .getNamedImports()
-          .map(namedImport => namedImport.getName())
+          .map((namedImport) => namedImport.getName());
         const uniqueNamedImports = Array.from(
           new Set([...existingNamedImports, ...namedImports]),
-        )
+        );
 
         // Remove all existing named imports and add the merged ones
-        existingImport.removeNamedImports()
-        existingImport.addNamedImports(uniqueNamedImports)
+        existingImport.removeNamedImports();
+        existingImport.addNamedImports(uniqueNamedImports);
       }
 
       // Add default import if it doesn't already exist
       if (defaultImport && !existingImport.getDefaultImport()) {
-        existingImport.setDefaultImport(defaultImport)
+        existingImport.setDefaultImport(defaultImport);
       }
-    }
-    else {
+    } else {
       // If no existing import, create a new one
       sourceFile.addImportDeclaration({
         namedImports,
         defaultImport,
         moduleSpecifier,
-      })
+      });
     }
   }
 }
@@ -82,13 +81,13 @@ export function addImports(sourceFile: SourceFile, imports: TsImports[]) {
  */
 export function addImportsToSourceFile(
   sourceFile: SourceFile,
-  imports: { imports: string | string[], from: string }[],
+  imports: { imports: string | string[]; from: string }[],
 ) {
   const modified = imports.map<TsImports>(({ from, imports }) => {
     if (Array.isArray(imports)) {
-      return { namedImports: imports, moduleSpecifier: from }
+      return { namedImports: imports, moduleSpecifier: from };
     }
-    return { defaultImport: imports, moduleSpecifier: from }
-  })
-  return addImports(sourceFile, modified)
+    return { defaultImport: imports, moduleSpecifier: from };
+  });
+  return addImports(sourceFile, modified);
 }
