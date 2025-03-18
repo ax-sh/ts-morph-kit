c1bdf6dac669df73b868625226b6c6e275c2c22d
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-18 16:26:12 +0545

    chore: lint remove semi columns

--- src/utils/find-default-export.ts
+++ src/utils/find-default-export.ts
@@ -1,14 +1,14 @@
-import type { SourceFile } from "ts-morph"
+import type { SourceFile } from "ts-morph";
 
 export function findDefaultExport(sourceFile: SourceFile) {
   // Find the export default declaration
   const exportDefault = sourceFile
     .getExportAssignments()
-    .find(assignment => !assignment.isExportEquals())
+    .find((assignment) => !assignment.isExportEquals());
   if (!exportDefault) {
-    throw new Error("No 'export default' found in the source file.")
+    throw new Error("No 'export default' found in the source file.");
   }
   // console.log("Found 'export default':");
-  const exportedExpression = exportDefault.getExpression()
-  return exportedExpression
+  const exportedExpression = exportDefault.getExpression();
+  return exportedExpression;
 }
