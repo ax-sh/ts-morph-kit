0c440fc32e1817650bb3223e2304080d9aba822e
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-17 15:07:32 +0545

    style: Format code and update dependency version

--- knip.config.ts
+++ knip.config.ts
@@ -1,13 +1,23 @@
 import type { KnipConfig } from "knip"
 
 const config: KnipConfig = {
-  ignore: ["src/components/ui/**", "src/routeTree.gen.ts", "src/utils/test-utils.ts"], // : ['createMemorySourceFile'],
-  ignoreDependencies: ["@vitest/coverage-v8", "@biomejs/biome", "@release-it/conventional-changelog", "is-ci", "git-cliff", "oxlint"],
+  ignore: [
+    "src/components/ui/**",
+    "src/routeTree.gen.ts",
+    "src/utils/test-utils.ts",
+  ], // : ['createMemorySourceFile'],
+  ignoreDependencies: [
+    "@vitest/coverage-v8",
+    "@biomejs/biome",
+    "@release-it/conventional-changelog",
+    "is-ci",
+    "git-cliff",
+    "oxlint",
+  ],
   ignoreBinaries: ["nr", "repo"],
   ignoreExportsUsedInFile: {
     interface: true,
     type: true,
-
   },
 }
 
