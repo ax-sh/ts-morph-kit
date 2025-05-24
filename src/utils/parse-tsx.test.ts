e235b9d558846cde8663fa0458212545a0733bfd
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-24 16:51:11 +0545

    chore(deps): Update dependency renovate to v40 (#4)
    * test: add TSX mutation tests and utility functions
    
    * test(parse-tsx): add tests for wrapWithTag function
    
    * test(parse-tsx): update test tag for jsx element wrapping
    
    * build: update Renovate GitHub Action version to 39.2.4
    
    * build: update renovate dependency version to 39.257.5
    
    * chore(deps): Update dependency renovate to v40
    
    ---------
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

---
+++ src/utils/parse-tsx.test.ts
@@ -0,0 +1,58 @@
+import type { JsxElement } from "ts-morph";
+import { SyntaxKind } from "ts-morph";
+import { createTestTsxSourceFile } from "./test-utils.ts";
+
+const code = `
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+`
+
+function wrapWithTag(jsxElement: JsxElement, tag: string) {
+  const newJsxText = `
+    <${tag}>
+      ${jsxElement.getText()}
+    </${tag}>
+  `;
+  jsxElement.getSourceFile().formatText();
+  // Replace the original JSX element with the new wrapped version
+  return jsxElement.replaceWithText(newJsxText);
+}
+
+describe('tsx mutate test', () => {
+  // it('should tsx mutate test', async () => {
+  //   const sourceFile = await createTestSourceFile(code)
+  //   const returnStatement = sourceFile.getFirstDescendantByKind(SyntaxKind.ReturnStatement);
+  //   const jsxElement = returnStatement?.getFirstDescendantByKind(SyntaxKind.JsxElement);
+  //   if (jsxElement) {
+  //
+  //     const text = jsxElement.getText();
+  //     console.log(text)
+  //     expect(text).toBeDefined()
+  //   }
+  //
+  // });
+  it('should wrap with custom tsx tag', async () => {
+    const sourceFile = await createTestTsxSourceFile(code)
+    // Find the return statement containing the JSX
+    const returnStatement = sourceFile.getFirstDescendantByKind(SyntaxKind.ReturnStatement);
+    const jsxElement = returnStatement?.getFirstDescendantByKind(SyntaxKind.JsxElement);
+
+    if (jsxElement) {
+      const tag = '__Test'
+      const r = wrapWithTag(jsxElement, tag);
+      console.log(r.getText())
+    }// Format the document to ensure proper indentation
+
+    const xml = sourceFile.getFirstDescendantByKind(SyntaxKind.ReturnStatement)?.getText()
+
+    console.log(xml);
+  });
+});
